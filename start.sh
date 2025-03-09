2d53b357ea602e43e06d09b73ad9423f48dbc66c
joao994 <joaovitor994@hotmail.com>
2025-03-09 00:31:20 -0300

    subindo

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Iniciando aplicação Dashboard Spot..."
-echo "Verificando arquivos estáticos..."
-ls -la /app/public
-
-echo "Iniciando servidor Node.js..."
-node api/server.js 
